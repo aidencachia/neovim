a02d9659037d3a2a5fb07b838a1164a24306cb72
aidencachia <aiden.cachia.23@um.edu.mt>
2024-08-19 13:34:36 +0200

    Removed Outdated File Explorer Keybind and Added Window Keybinds

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,2 +1,13 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Window Traversial
+vim.keymap.set("n", "<C-h>",'<Cmd>wincmd h<Cr>')
+vim.keymap.set("n", "<C-l>",'<Cmd>wincmd l<Cr>')
+vim.keymap.set("n", "<C-j>",'<Cmd>wincmd j<Cr>')
+vim.keymap.set("n", "<C-k>",'<Cmd>wincmd k<Cr>')
+
+-- Window Creation
+vim.keymap.set("n", "<C-S-h>",'<Cmd>vsplit<Cr>')
+vim.keymap.set("n", "<C-S-l>",'<Cmd>vsplit<Cr>')
+vim.keymap.set("n", "<C-S-J>",'<Cmd>split<Cr>')
+vim.keymap.set("n", "<C-S-k>",'<Cmd>split<Cr>')

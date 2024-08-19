773fe51778898c0a186454b6540501349feb00d1
aidencachia <aiden.cachia.23@um.edu.mt>
2024-08-19 13:56:16 +0200

    Added live-grep-args keybinding

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,5 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+

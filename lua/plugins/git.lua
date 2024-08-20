4ec7f9c6fbad3202b75df849955c289bbdbbe852
Aiden Cachia <aidenc@ccbill.com>
2024-08-20 11:16:14 +0200

    Added new gitlab plugin

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,4 +1,24 @@
 return {
-    "sodapopcan/vim-twiggy",
-    dependencies = {"tpope/vim-fugitive"}
+    -- Branches
+    {
+        "sodapopcan/vim-twiggy",
+        dependencies = {"tpope/vim-fugitive"}
+    },
+    -- Gitlab
+    {
+        "harrisoncramer/gitlab.nvim",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "nvim-lua/plenary.nvim",
+            "sindrets/diffview.nvim",
+            "stevearc/dressing.nvim", -- Recommended but not required. Better UI for pickers.
+            "nvim-tree/nvim-web-devicons" -- Recommended but not required. Icons in discussion tree.
+        },
+        enabled = true,
+        build = function () require("gitlab.server").build(true) end, -- Builds the Go binary
+        config = function()
+            require("gitlab").setup()
+        end,
+    }
+
 }

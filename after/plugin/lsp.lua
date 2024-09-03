fffad1e535988565d4f5526cb69848b35682dc0b
Aiden Cachia <aidenc@ccbill.com>
2024-09-03 14:58:06 +0200

    Disable Java Plugins as it is giving errors

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,6 +1,8 @@
 local lsp_zero = require('lsp-zero')
 
-local lsp_attach = function(client, bufnr)
+local path = require('path');
+
+local lsp_attach = function(_, bufnr)
   local opts = {buffer = bufnr}
 
   vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
@@ -16,15 +18,22 @@ local lsp_attach = function(client, bufnr)
 end
 
 lsp_zero.extend_lspconfig({
-  sign_text = true,
+    sign_text = true,
   lsp_attach = lsp_attach,
   capabilities = require('cmp_nvim_lsp').default_capabilities()
 })
 
-require('java').setup()
-require('lspconfig').jdtls.setup({})
-require('jdtls').test_class()
-require('jdtls').test_nearest_method()
+-- require('java').setup()
+-- require('lspconfig').jdtls.setup({})
+-- 
+-- local config = {
+--     cmd = {path.."etc/eclipse-jdtls/bin/jdtls"},
+--     root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+-- }
+-- require('jdtls').start_or_attach(config)
+-- 
+-- require('jdtls').test_class()
+-- require('jdtls').test_nearest_method()
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
@@ -38,57 +47,52 @@ require('mason-lspconfig').setup({
 local cmp = require('cmp')
 
 cmp.setup({
-  sources = {
-    {name = 'nvim_lsp'},
-  },
-  snippet = {
-    expand = function(args)
-      -- You need Neovim v0.10 to use vim.snippet
-      vim.snippet.expand(args.body)
-    end,
-  },mapping = {
-    ['<cr>'] = cmp.mapping.confirm({select = false}),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<C-k>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
-    ['<C-j>'] = cmp.mapping.select_next_item({behavior = 'select'}),
-    ['<C-p>'] = cmp.mapping(function()
-      if cmp.visible() then
-        cmp.select_prev_item({behavior = 'insert'})
-      else
-        cmp.complete()
-      end
-    end),
-    ['<C-n>'] = cmp.mapping(function()
-      if cmp.visible() then
-        cmp.select_next_item({behavior = 'insert'})
-      else
-        cmp.complete()
-      end
-    end),
-  },
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
+    sources = {
+        {name = 'nvim_lsp'},
+    },
+    snippet = {
+        expand = function(args)
+            -- You need Neovim v0.10 to use vim.snippet
+            vim.snippet.expand(args.body)
+            require('luasnip').lsp_expand(args.body)
+        end,
+    },mapping = {
+        ['<cr>'] = cmp.mapping.confirm({select = false}),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<C-k>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+        ['<C-j>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+        ['<C-p>'] = cmp.mapping(function()
+            if cmp.visible() then
+                cmp.select_prev_item({behavior = 'insert'})
+            else
+                cmp.complete()
+            end
+        end),
+        ['<C-n>'] = cmp.mapping(function()
+            if cmp.visible() then
+                cmp.select_next_item({behavior = 'insert'})
+            else
+                cmp.complete()
+            end
+        end),
+    },
     completion = { completeopt = 'menu,menuone,noinsert' }
 })
 
-local path = require('path');
 
 -- This bundles definition is the same as in the previous section (java-debug installation)
-local bundles = {
-  vim.fn.glob(path.."etc/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar", 1),
-};
+-- local bundles = {
+--   vim.fn.glob(path.."etc/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar"),
+-- };
+-- 
+-- -- This is the new part
+-- vim.list_extend(bundles, vim.split(vim.fn.glob(path.."etc/vscode-java-test/server/*.jar"), "\n"))
+-- local config ={
+--     init_options = {
+--         bundles = bundles;
+--     }
+-- }
 
--- This is the new part
-vim.list_extend(bundles, vim.split(vim.fn.glob(path.."etc/vscode-java-test/server/*.jar", 1), "\n"))
-local config ={
-    init_options = {
-        bundles = bundles;
-    }
-}
-
-require('jdtls').start_or_attach(config)
+-- require('jdtls').start_or_attach(config)
 
 
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -23,6 +23,6 @@ return {
     {'rafamadriz/friendly-snippets'},
 
     -- Java
-    {'nvim-java/nvim-java'},
-    {'mfussenegger/nvim-jdtls'}
+    -- {'nvim-java/nvim-java'},
+    -- {'mfussenegger/nvim-jdtls'}
 }

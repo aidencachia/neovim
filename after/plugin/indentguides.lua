345b68d971d3d84503a1cae66be194b23c03b814
aidenc-ccbill <aidenc@ccbill.com>
2025-07-21 11:42:37 +0300

    Make color of normal indent guide more dark

--- after/plugin/indentguides.lua
+++ after/plugin/indentguides.lua
@@ -1,12 +1,14 @@
 local hooks = require "ibl.hooks"
 
 hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+  vim.api.nvim_set_hl(0, "IblNormal", { fg = "#444444" })
   vim.api.nvim_set_hl(0, "IblScope", { fg = "#00ffff" })
 end)
 
 require("ibl").setup {
   indent = {
-    char = '▏'
+    char = '▏',
+    highlight = "IblNormal"
   },
   scope = {
     enabled = true,
